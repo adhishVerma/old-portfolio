9856c0b9cbe0ab5a3e4f9597d1fbe4c2e690aac6
adhishVerma <v.adhish16@gmail.com>
2023-03-17 18:41:44 +0530

    add:project grid

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-(function(){
-
+(function () {
   var doc = document.documentElement;
   var w = window;
 
@@ -8,21 +7,19 @@
   var direction = 0;
   var prevDirection = 0;
 
-  var header = document.getElementById('site-header');
+  var header = document.getElementById("site-header");
 
-  var checkScroll = function() {
-
+  var checkScroll = function () {
     /*
-    ** Find the direction of scroll
-    ** 0 - initial, 1 - up, 2 - down
-    */
+     ** Find the direction of scroll
+     ** 0 - initial, 1 - up, 2 - down
+     */
 
     curScroll = w.scrollY || doc.scrollTop;
-    if (curScroll > prevScroll) { 
+    if (curScroll > prevScroll) {
       //scrolled up
       direction = 2;
-    }
-    else if (curScroll < prevScroll) { 
+    } else if (curScroll < prevScroll) {
       //scrolled down
       direction = 1;
     }
@@ -30,24 +27,59 @@
     if (direction !== prevDirection) {
       toggleHeader(direction, curScroll);
     }
-    
+
     prevScroll = curScroll;
   };
 
-  var toggleHeader = function(direction, curScroll) {
-    if (direction === 2 && curScroll > 52) { 
-      
+  var toggleHeader = function (direction, curScroll) {
+    if (direction === 2 && curScroll > 52) {
       //replace 52 with the height of your header in px
 
-      header.classList.add('hide');
+      header.classList.add("hide");
       prevDirection = direction;
-    }
-    else if (direction === 1) {
-      header.classList.remove('hide');
+    } else if (direction === 1) {
+      header.classList.remove("hide");
       prevDirection = direction;
     }
   };
-  
-  window.addEventListener('scroll', checkScroll);
 
+  window.addEventListener("scroll", checkScroll);
 })();
+
+const projects = document.getElementById("project-grid");
+
+for (let i = 0; i < 16; i++) {
+  let card = `<div class="card" style="background:hsl(${
+    Math.random() * 360
+  }, 100%, 50%)">
+  <img src="http://source.unsplash.com/random/?sig=${i}" alt=""/>
+  </div>`;
+
+  projects.innerHTML += card;
+}
+
+projects.addEventListener("mousemove", (e) => {
+  const mouseX = e.clientX;
+  const mouseY = e.clientY;
+  
+  const decX = mouseX/window.innerWidth
+  const decY = mouseY/window.innerHeight
+  const maxX = projects.offsetWidth - window.innerWidth
+  const maxY = projects.offsetHeight - window.innerHeight
+
+  const panX = (-1)*maxX*decX
+  const panY = (-1)*maxY*decY
+
+
+  projects.animate(
+    {
+      translate:`${panX}px ${panY}px`
+    },
+    {
+      duration:3000,
+      easing:"ease",
+      fill:"forwards"
+    }
+  )
+
+});
