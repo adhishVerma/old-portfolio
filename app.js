3c0a4ca2e30175b9551bc187b99bea160242c7bc
adhishVerma <v.adhish16@gmail.com>
2023-08-08 02:01:27 +0530

    update: content

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// nav-bar
 (function () {
   var doc = document.documentElement;
   var w = window;
@@ -48,60 +49,94 @@
 
 const projects = document.getElementById("project-grid");
 
-fetch("./static/data.json").then((res) => res.json()).then(data => 
-    {
-        data.forEach((project, i) => {
-            
-                let card = `<div class="card" style="background:hsl(${
-                  Math.random() * 360
-                }, 100%, 50%)">
-                <img src="http://source.unsplash.com/random/?sig=${i}" alt=""/>
+fetch("./static/data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((project, i) => {
+      let card = `<div class="card">
+                <div class="img-container">
+                <img src="${project.img}" alt="" loading="lazy"/></div>
                 <div class="project-info">
-                <p class="project-title">${project.title}</p>
-                <div class="project-link"><a href="${project.link}" target="blank_">Project-link</a></div></div>
+                <p class="project-title"><a href="${project.link}" target="blank_">${project.title}</a></p>
+                <div class="project-link"><a href="${project.github}" target="blank_">Github <i class="fa-brands fa-github fa-sm" style="color: #f9f9f9;"></i></a></div></div>
                 </div>`;
-              
-                projects.innerHTML += card;
-              
-        })
+
+      projects.innerHTML += card;
+    });
+  });
+
+// tab-switching
+
+const onTabClick = (event) => {
+  let activeTabs = document.querySelectorAll(".active");
+
+  activeTabs.forEach((activeTab) => {
+    activeTab.classList.remove("active");
+  });
+
+  event.target.parentElement.className += " active";
+  document.getElementById(event.target.href.split("#")[1]).classList +=
+    " active";
+};
+
+const navTab = document.getElementById("nav-tab");
+
+navTab.addEventListener("click", (e) => onTabClick(e), false);
+
+// contact link animation
+const contactLink = document.querySelector("#contact-link");
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+contactLink.addEventListener("mouseover", (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
     }
-    )
 
-// for (let i = 0; i < 7; i++) {
-//   let card = `<div class="project-card-container"><div class="card" style="background:hsl(${
-//     Math.random() * 360
-//   }, 100%, 50%)">
-//   <img src="http://source.unsplash.com/random/?sig=${i}" alt=""/>
-//   </div>
-//   <p class="project-title">lorem ipsum dolor sit</p>
-//   <div class="project-link">Project Link</div>
-//   </div>`;
+    iteration += 1 / 2;
+  }, 30);
+});
 
-//   projects.innerHTML += card;
-// }
-
-projects.addEventListener("mousemove", (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  
-  const decX = mouseX/window.innerWidth
-  const decY = mouseY/window.innerHeight
-  const maxX = (projects.offsetWidth - window.innerWidth)
-  const maxY = (projects.offsetHeight - window.innerHeight)/2.5
-
-  const panX = (-1)*maxX*decX
-  const panY = (-1)*maxY*decY
+gsap.from("#hello", {
+  x: "-100vw",
+  duration: 1.3,
+  scale : 0
+});
 
 
-  projects.animate(
-    {
-      translate:`${panX}px ${panY}px`
-    },
-    {
-      duration:3000,
-      easing:"ease",
-      fill:"forwards"
-    }
-  )
+gsap.from(".jobTitle", {
+  x: "100vw",
+  scale : 0,
+  duration: 1.3,
+});
 
+
+gsap.from("#scroll-down", {
+  x: "-100vw",
+  duration: 2,
+});
+
+
+
+gsap.to("#scroll-down", {
+  delay: 3.7,
+  duration: 1.2,
+  fontSize: "0.5em",
+  y : "15em"
 });
